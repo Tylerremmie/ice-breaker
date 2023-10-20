939fa8250e13836b494fa6818d11c0819b9de24a
Tyler Remmie <tylerremmie@gmail.com>
2023-10-19 22:39:53 -0500

    Pushing code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import { ThemePicker } from './components/themePicker/ThemePicker';
 
 function App() {
   return (
     <div className="App">
       <div className='Component-Container'>
-        <ThemePicker />
+        <div>TEST</div>
       </div>
     </div>
   );
